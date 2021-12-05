dc9c1e035e2ac9746b230fec99ee39392d9ae32a
Karolina Zdziarska <Karolina.Zdziarska@britishland.com>
2021-12-05 17:59:19 +0000

    Added Product class with string formatting unit tests

---
+++ PriceCalculator.Tests/ProductTests/ToStringTests.cs
@@ -0,0 +1,38 @@
+﻿using PriceCalculator.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PriceCalculator.Tests.ProductTests
+{
+    public class ToStringTests
+    {
+        [Fact]
+        public void ToString_PriceWithNoDecimalPlaces_ShouldReturnCorrectString()
+        {
+            var product = new Product("Milk", 2);
+            var productAsString = product.ToString();
+
+            Assert.Equal("Name: Milk | Cost: £2.00", productAsString);
+        }
+
+        [Fact]
+        public void ToString_PriceWithOneDecimalPlace_ShouldReturnCorrectString()
+        {
+            var product = new Product("Butter", 2.5m);
+            var productAsString = product.ToString();
+
+            Assert.Equal("Name: Butter | Cost: £2.50", productAsString);
+        }
+
+        [Fact]
+        public void ToString_PriceWithTwoDecimalPlaces_ShouldReturnCorrectString()
+        {
+            var product = new Product("Water", 2.59m);
+            var productAsString = product.ToString();
+
+            Assert.Equal("Name: Water | Cost: £2.59", productAsString);
+        }
+    }
+}
---
+++ PriceCalculator/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceCalculator.Entities
+{
+    public class Product
+    {
+        public string Name { get; private set; }
+        public decimal Cost { get; private set; }
+
+        public Product(string name, decimal cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name} | Cost: £{Cost:#.00}";
+        }
+    }
+}
